09599d33e6107a9510d094725f706abc9bd43d14
Martin Coronel <martincxx@gmail.com>
2018-01-05 13:02:12 +0300

    Create exercise7.py

---
+++ PythonforAstronomers/exercise7.py
@@ -0,0 +1,16 @@
+"""
+7. Write a program able to play the "Guess the number"-game, where the number to be guessed is randomly chosen between 1 and 20. (Source: http://inventwithpython.com) This is how it should work when run in a terminal:
+Output of the program (example):
+Hello! What is your name?
+Oak
+Well, Oak, I am thinking of a number between 1 and 20.
+Take a guess.
+10
+Your guess is too low.
+Take a guess.
+15
+Your guess is too low.
+Take a guess.
+18
+Good job, Oak You guessed my number in 3 guesses!
+"""
