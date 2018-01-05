a3a672dedda10023c7f2be0336f8078760525440
Martin Coronel <martincxx@gmail.com>
2018-01-05 13:29:49 +0300

    Update exercise7.py
    Code corrected

--- PythonforAstronomers/exercise7.py
+++ PythonforAstronomers/exercise7.py
@@ -15,3 +15,23 @@ Take a guess.
 18
 Good job, Oak You guessed my number in 3 guesses!
 """
+from sys import stdout
+from random import randint
+stdout.write("Lets play!\n")
+x = input("Hello! What is your name?: ")
+
+stdout.write("Well "+x+ ", I am thinking of a number between 1 and 20\n")
+secret =randint(1, 20)
+guess=0
+time=1
+while guess!=secret:
+    guess = input("Take a guess: ")
+    g=int(guess)
+    if g==secret:
+        stdout.write("Good job "+x+ "! You guessed my number in "+str(time)+ " guesses!\n")        
+        break
+    elif g<secret:
+        stdout.write("Your guess is too low.\n")
+    else:
+        stdout.write("Your guess is too high.\n")
+    time+=1
