fff9009409716f6f5049841a97edbd77f1db79a6
Martin Coronel <martincxx@gmail.com>
2018-01-05 12:14:07 +0300

    Update exercise3_1.py
    Includes reading word from console using sys

--- PythonforAstronomers/exercise3_1.py
+++ PythonforAstronomers/exercise3_1.py
@@ -1,1 +1,13 @@
+def is_palindrome(word):
+    backwards=word[::-1]
+    if word==backwards:
+        return True
+    else:
+        return False
 
+#read from console
+import sys
+sys.stdout.write("Another way to do it!\n")
+x = input("Word to examine: ")
+y = is_palindrome(str(x))
+sys.stdout.write(str(y))
