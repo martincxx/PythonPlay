1ba1b03b0aa8e9c1f31e2d1d4b4b2f2192163059
Martin Coronel <martincxx@gmail.com>
2018-01-13 11:18:43 +0300

    Update Lab2.py

--- DataAnalysisCognitiveClass/Lab2.py
+++ DataAnalysisCognitiveClass/Lab2.py
@@ -1,1 +1,178 @@
-#oak
+import pandas as pd
+
+#location of data
+filename = 'https://archive.ics.uci.edu/ml/machine-learning-databases/autos/imports-85.data'
+
+#headers for data
+
+headers = ["symboling","normalized-losses","make","fuel-type","aspiration", "num-of-doors","body-style",
+         "drive-wheels","engine-location","wheel-base", "length","width","height","curb-weight","engine-type",
+         "num-of-cylinders", "engine-size","fuel-system","bore","stroke","compression-ratio","horsepower",
+         "peak-rpm","city-mpg","highway-mpg","price"]
+
+#read the data and assign headers for df
+df = pd.read_csv(filename, names = headers)
+
+#check data 
+df.head()
+
+# since there are many missing values, we replace "?" to NaN
+df.replace("?", np.nan, inplace = True)
+df.head(5)
+
+
+#check null values
+
+missing_data = df.isnull()
+missing_data.head(5)
+
+#count missing values in each column
+
+for column in missing_data.columns.values.tolist():
+    print(column)
+    print (missing_data[column].value_counts())
+    print("")    
+
+#we can replace null values with the average of column
+
+Calculate the average of the column
+avg_1 = df["normalized-losses"].astype("float").mean(axis = 0)
+
+df["normalized-losses"].replace(np.nan, avg_1, inplace = True)
+
+avg_2=df['bore'].astype('float').mean(axis=0)
+df['bore'].replace(np.nan, avg_2, inplace= True)
+
+# calculate the mean vaule for "stroke" column
+avg_3 = df["stroke"].astype("float").mean(axis = 0)
+
+# replace NaN by mean value in "stroke" column
+df["stroke"].replace(np.nan, avg_3, inplace = True)
+
+avg_4=df['horsepower'].astype('float').mean(axis=0)
+df['horsepower'].replace(np.nan, avg_4, inplace= True)
+
+avg_5=df['peak-rpm'].astype('float').mean(axis=0)
+df['peak-rpm'].replace(np.nan, avg_5, inplace= True)
+
+#check number of each value in a column 
+df['num-of-doors'].value_counts()
+
+#finding out which value is the most common
+df['num-of-doors'].value_counts().idxmax()
+
+
+#replace the missing 'num-of-doors' values by the most frequent 
+df["num-of-doors"].replace(np.nan, "four", inplace = True)
+
+# simply drop whole row with NaN in "price" column
+df.dropna(subset=["price"], axis=0, inplace = True)
+
+# reset index, because we droped two rows
+df.reset_index(drop = True, inplace = True)
+
+#In Pandas, we use
+#.dtype() to check the data type
+#.astype() to change the data type
+
+#Lets list the data types for each column
+
+df.dtypes
+
+#change format type of columns
+
+df[["bore", "stroke"]] = df[["bore", "stroke"]].astype("float")
+df[["normalized-losses"]] = df[["normalized-losses"]].astype("int")
+df[["price"]] = df[["price"]].astype("float")
+df[["peak-rpm"]] = df[["peak-rpm"]].astype("float")
+
+# transform mpg to L/100km by mathematical operation (235 divided by mpg)
+df['city-L/100km'] = 235/df["city-mpg"]
+
+# check your transformed data 
+df.head()
+
+# transform mpg to L/100km by mathematical operation (235 divided by mpg)
+df["highway-mpg"] = 235/df["highway-mpg"]
+
+# rename column name from "highway-mpg" to "highway-L/100km"
+df.rename(columns={'"highway-mpg"':'highway-L/100km'}, inplace=True)
+
+# check your transformed data 
+df.head()
+
+# replace (original value) by (original value)/(maximum value)
+df['length'] = df['length']/df['length'].max()
+df['width'] = df['width']/df['width'].max()
+df['height'] = df['height']/df['height'].max() 
+# show the scaled columns
+df[["length","width","height"]].head()
+
+#Binning data in Pandas
+df["horsepower"]=df["horsepower"].astype(float, copy=True)
+binwidth = (max(df["horsepower"])-min(df["horsepower"]))/4
+bins = np.arange(min(df["horsepower"]), max(df["horsepower"]), binwidth)
+#group names for our bin
+group_names = ['Low', 'Medium', 'High']
+df['horsepower-binned'] = pd.cut(df['horsepower'], bins, labels=group_names,include_lowest=True )
+df[['horsepower','horsepower-binned']].head(20)
+
+#this magic is for python notepads
+%matplotlib inline
+import matplotlib as plt
+from matplotlib import pyplot
+
+a = (0,1,2)
+
+# draw historgram of attribute "horsepower" with bins = 3
+plt.pyplot.hist(df["horsepower"], bins = 3)
+
+# set x/y labels and plot title
+plt.pyplot.xlabel("horsepower")
+plt.pyplot.ylabel("count")
+plt.pyplot.title("horsepower bins")
+
+
+#We will use the pandas method 'get_dummies' to assign numerical values to different categories of fuel type. 
+
+# dummy_1 = pd.get_dummies(df["column-name"])
+
+dummy_variable_1 = pd.get_dummies(df["fuel-type"])
+
+# change column names for clarity
+dummy_variable_1.rename(columns={'fuel-type-diesel':'gas', 'fuel-type-diesel':'diesel'}, inplace=True)
+
+# show first 5 instances of data frame "dummy_variable_1"
+dummy_variable_1.head()
+
+# merge data frame "df" and "dummy_variable_1" 
+df = pd.concat([df, dummy_variable_1], axis=1)
+
+# drop original column "fuel-type" from "df"
+df.drop("fuel-type", axis = 1, inplace=True)
+
+# get indicator variables of aspiration and assign it to data frame "dummy_variable_2"
+dummy_variable_2 = pd.get_dummies(df['aspiration'])
+
+# change column names for clarity
+dummy_variable_2.rename(columns={'std':'aspiration-std', 'turbo': 'aspiration-turbo'}, inplace=True)
+
+# show first 5 instances of data frame "dummy_variable_1"
+dummy_variable_2.head()
+
+# get indicator variables of aspiration and assign it to data frame "dummy_variable_2"
+dummy_variable_2 = pd.get_dummies(df['aspiration'])
+
+# change column names for clarity
+dummy_variable_2.rename(columns={'std':'aspiration-std', 'turbo': 'aspiration-turbo'}, inplace=True)
+
+# show first 5 instances of data frame "dummy_variable_1"
+dummy_variable_2.head()
+
+#merge the new dataframe to the original dataframe
+df = pd.concat([df, dummy_variable_2], axis=1)
+
+# drop original column "aspiration" from "df"
+df.drop('aspiration', axis = 1, inplace=True)
+#save clean csv
+df.to_csv('clean_df.csv')
